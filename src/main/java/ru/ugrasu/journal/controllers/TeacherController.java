9189d25ca98930f5e336ca0cd5ea4deb9acad06b
DESKTOP-G8C0V64\Алексей <alekseychernavskiy@gmail.com>
2017-12-13 01:17:40 +0500

    Получаю предметы, группы, студентов. - Студенты привязываются к группе в JS. Надо отправлять на сервер группу и там искать, чтобы клиент только принимал, иначе логика неправильно построена - Сделать привязку групп к предмету

--- src/main/java/ru/ugrasu/journal/controllers/TeacherController.java
+++ src/main/java/ru/ugrasu/journal/controllers/TeacherController.java
@@ -3,10 +3,15 @@ package ru.ugrasu.journal.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.ugrasu.journal.dto.RoleDto;
+import ru.ugrasu.journal.dto.StudyGroupDto;
+import ru.ugrasu.journal.dto.SubjectDto;
 import ru.ugrasu.journal.dto.UserDto;
-import ru.ugrasu.journal.model.entities.RoleEntity;
-import ru.ugrasu.journal.model.services.RoleService;
+import ru.ugrasu.journal.model.entities.StudyGroupEntity;
+import ru.ugrasu.journal.model.entities.SubjectEntity;
+import ru.ugrasu.journal.model.entities.UserEntity;
+import ru.ugrasu.journal.model.services.StudyGroupService;
+import ru.ugrasu.journal.model.services.SubjectService;
+import ru.ugrasu.journal.model.services.UserService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,40 +24,91 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 public class TeacherController {
 
     @Autowired
-    private RoleService roleService;
+    private SubjectService subjectService;
 
-    @RequestMapping(value = "/refrash", produces = APPLICATION_JSON_UTF8_VALUE, method = GET)
-    public List<RoleDto> findAll() {
-        System.out.println("RoleController - findAll");
+    @Autowired
+    private StudyGroupService studyGroupService;
 
-        List<RoleDto> listRoleDto = new ArrayList<>();
-        List<RoleEntity> listRoleEntity = roleService.findAll();
+    @Autowired
+    private UserService userService;
 
-        if (listRoleEntity == null) {
-            throw new RuntimeException("No roles? Impossible to see that error");
+    @RequestMapping(value = "/refrashSubject", produces = APPLICATION_JSON_UTF8_VALUE, method = GET)
+    public List<SubjectDto> findAllSubject() {
+        System.out.println("TeacherController - findAllSubject");
+
+        List<SubjectDto> listSubjectDto = new ArrayList<>();
+        List<SubjectEntity> listSubjectEntity = subjectService.findAll();
+
+        if (listSubjectEntity == null) {
+            throw new RuntimeException("No groups");
         }
         else {
-            listRoleEntity.forEach(roleEntity -> {
-                RoleDto roleDto = new RoleDto();
-                roleDto.setId(roleEntity.getId());
-                roleDto.setName(roleEntity.getName());
+            listSubjectEntity.forEach(subjectEntity -> {
+                SubjectDto subjectDto = new SubjectDto();
+                subjectDto.setId(subjectEntity.getId());
+                subjectDto.setName(subjectEntity.getName());
 
-                List<UserDto> listUserDto = new ArrayList<>();
+                listSubjectDto.add(subjectDto);
+            });
 
-                roleEntity.getUsersById().forEach(userEntity -> {
+            return listSubjectDto;
+        }
+    }
+
+    @RequestMapping(value = "/refrashStudyGroup", produces = APPLICATION_JSON_UTF8_VALUE, method = GET)
+    public List<StudyGroupDto> findAllStudyGroup() {
+        System.out.println("TeacherController - findAllStudyGroup");
+
+        List<StudyGroupDto> listStudyGroupDto = new ArrayList<>();
+        List<StudyGroupEntity> listStudyGroupEntity = studyGroupService.findAll();
+
+        if (listStudyGroupEntity == null) {
+            throw new RuntimeException("No groups");
+        }
+        else {
+            listStudyGroupEntity.forEach(studyGroupEntity -> {
+                StudyGroupDto studyGroupDto = new StudyGroupDto();
+                studyGroupDto.setId(studyGroupEntity.getId());
+                studyGroupDto.setName(studyGroupEntity.getName());
+
+                listStudyGroupDto.add(studyGroupDto);
+            });
+
+            return listStudyGroupDto;
+        }
+    }
+
+    @RequestMapping(value = "/getStudents", produces = APPLICATION_JSON_UTF8_VALUE, method = GET)
+    public List<UserDto> findAllUser() {
+        System.out.println("TeacherController - findAllUser");
+
+        List<UserDto>       listUserDto = new ArrayList<>();
+        List<UserEntity> listUserEntity = userService.findAll();
+
+        if (listUserEntity == null) {
+            throw new RuntimeException("No groups");
+        }
+        else {
+            listUserEntity.forEach(userEntity -> {
+
+                //Проверка роли (студент или староста)
+                String role = userEntity.getRoleByRole().getName();
+                if (role.equals("Староста") || role.equals("Студент")) {
                     UserDto userDto = new UserDto();
                     userDto.setId(userEntity.getId());
                     userDto.setName(userEntity.getName());
+                    userDto.setStudyGroupByStudyGroup(userEntity.getStudyGroupByStudyGroup().getName());
 
                     listUserDto.add(userDto);
-
-                });
-
-                roleDto.setUsersById(listUserDto);
-                listRoleDto.add(roleDto);
+                }
             });
 
-            return listRoleDto;
+            if (listUserDto == null) {
+                throw new RuntimeException("No students");
+            }
+            else {
+                return listUserDto;
+            }
         }
     }
 }
