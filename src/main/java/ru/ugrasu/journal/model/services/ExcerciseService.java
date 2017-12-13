b78ff4dda999f637938129faa04138714a17cee5
DESKTOP-G8C0V64\Алексей <alekseychernavskiy@gmail.com>
2017-12-13 08:31:24 +0500

    Ура, ура, страничка преподавателя закончена: - Просто возвращаем все занятия (студенты могут и не прийти, так что вернуть надо даже не заполненные студентами)

--- src/main/java/ru/ugrasu/journal/model/services/ExcerciseService.java
+++ src/main/java/ru/ugrasu/journal/model/services/ExcerciseService.java
@@ -2,13 +2,20 @@ package ru.ugrasu.journal.model.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import ru.ugrasu.journal.model.entities.ExcerciseEntity;
 import ru.ugrasu.journal.model.repositories.DepartmentRepository;
 import ru.ugrasu.journal.model.repositories.ExcerciseRepository;
+
+import java.util.List;
 
 @Component
 public class ExcerciseService {
 
     @Autowired
     private ExcerciseRepository excerciseRepository;
+
+    public List<ExcerciseEntity> findAll() {
+        return excerciseRepository.findAll();
+    }
 
 }
