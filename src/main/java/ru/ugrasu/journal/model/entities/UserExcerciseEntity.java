3fa08d7b4d03eef0e7bc75882708d9989e1853f5
DESKTOP-G8C0V64\Алексей <alekseychernavskiy@gmail.com>
2017-12-13 05:01:37 +0500

    Ну кааак можно было допустить элементарную ошибку! Не называйте таблицы через тире в БД
    - Убрано ограничение на обязательно заполнение, поскольку может не быть занятий или на занятии может не быть студентов!

--- src/main/java/ru/ugrasu/journal/model/entities/UserExcerciseEntity.java
+++ src/main/java/ru/ugrasu/journal/model/entities/UserExcerciseEntity.java
@@ -6,13 +6,13 @@ import javax.persistence.*;
 
 @Data
 @Entity
-@Table(name = "user-excercise", schema = "journal", catalog = "")
+@Table(name = "user_excercise", schema = "journal", catalog = "")
 public class UserExcerciseEntity {
     @Id@Column(name = "id")
     private int id;
-    @ManyToOne@JoinColumn(name = "user", referencedColumnName = "id", nullable = false)
+    @ManyToOne@JoinColumn(name = "user", referencedColumnName = "id")
     private UserEntity userByUser;
-    @ManyToOne@JoinColumn(name = "excercise", referencedColumnName = "id", nullable = false)
+    @ManyToOne@JoinColumn(name = "excercise", referencedColumnName = "id")
     private ExcerciseEntity excerciseByExcercise;
 
 }
